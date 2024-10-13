d616338a9da3622c39fa9f35f295ae84099ffe7e
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-10-13 09:39:31 +0100

    upload commit

---
+++ app/Http/Controllers/IngredientController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Ingredient;
+
+class IngredientController extends Controller
+{
+    public function index()
+    {
+        $ingredients = Ingredient::all();
+        return view('ingredients.index', compact('ingredients'));
+    }
+
+    public function create()
+    {
+        return view('ingredients.create');
+    }
+
+    public function store(Request $request)
+    {
+        $ingredient = new Ingredient;
+        $ingredient->name = $request->input('name');
+        $ingredient->type = $request->input('type');
+        $ingredient->save();
+
+        return redirect()->route('recipes.index');
+    }
+
+    public function show($id)
+    {
+        $ingredient = Ingredient::findOrFail($id);
+        
+        return view('recipes.show', compact('ingredient'));
+    }
+
+    public function edit($id)
+    {
+        $ingredient = Ingredient::findOrFail($id);
+        
+        return view('ingredients.edit', compact('ingredient'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $ingredient = Ingredient::findOrFail($id);
+        $ingredient->name = $request->input('name');
+        $ingredient->type = $request->input('type');
+        $ingredient->save();
+
+        return redirect()->route('recipe.index');
+    }
+
+    public function delete($id)
+    {
+        $ingredient = Ingredient::findOrFail($id);
+        $ingredient->delete();
+
+        return redirect()->route('recipe.index');
+    }
+}
---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Recipe;
+
+class RecipeController extends Controller
+{
+    public function index()
+    {
+        $recipes = Recipe::all();
+        return view('recipes.index', compact('recipes'));
+
+    }
+
+    public function create()
+    {
+        return view('recipes.create');
+    }
+
+    public function store(Request $request)
+    {
+        $recipe = new Recipe;
+        $recipe->name = $request->input('name');
+        $recipe->time = $request->input('time');
+        $recipe->save();
+
+        return redirect()->route('recipes.index');
+    }
+
+    public function show($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        
+        return view('recipes.show', compact('recipe'));
+    }
+
+    public function edit($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        
+        return view('recipes.edit', compact('recipe'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->name = $request->input('name');
+        $recipe->time = $request->input('time');
+        $recipe->save();
+
+        return redirect()->route('recipes.index');
+    }
+
+    public function delete($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->delete();
+
+        return redirect()->route('recipes.index');
+    }
+}
